6e04953a1397944f11cd433f3ac86a8df4644332
Tim K <>
2022-12-22 16:16:13 +0100

    first commit

---
+++ DBMenuBarApp/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  DBMenuBarApp
+//
+//  Created by Tim Knothe on 22.12.22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    private var statusItem: NSStatusItem!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        apiCall()
+        
+        
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "1.circle", accessibilityDescription: "1")
+        }
+    }
+    
+    
+    func apiCall() {
+        
+        guard let url = URL(string: "https://iceportal.de/api1/rs/status") else{
+                return
+            }
+
+
+            let task = URLSession.shared.dataTask(with: url){
+                data, response, error in
+                
+                if let data = data, let string = String(data: data, encoding: .utf8){
+                    print(string)
+                }
+            }
+
+            task.resume()
+    }
+}
+
---
+++ DBMenuBarApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DBMenuBarApp
+//
+//  Created by Tim Knothe on 22.12.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
